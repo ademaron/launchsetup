8335d684c7ddc50df8f434745489ac86f68e6ce5
ademaron <ademaron36@gmail.com>
2015-10-04 12:13:49 -0500

    Changed launch.sh commit

--- launch.sh
+++ launch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 aws ec2 run-instances --image-id $1 --count $2 
---instance-type $3 --key-name itmo444-ade-fall2015-thisone 
---security-group-ids $4 --subnet-id $5 
+--instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address 
